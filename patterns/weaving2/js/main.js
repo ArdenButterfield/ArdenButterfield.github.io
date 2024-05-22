9b3b9b1258f89dd94d77ae634ef9cfba98ffc761
Arden Butterfield <glogfogle@gmail.com>
2024-05-22 10:26:56 -0700

    full pattern

--- patterns/weaving2/js/main.js
+++ patterns/weaving2/js/main.js
@@ -4,7 +4,7 @@ function setup() {
 }
 
 const STEP = 40;
-const INCR = 0.3;
+const INCR = 0.15;
 let progression = 0;
 const SQUARE_SIZE = 250;
 function draw() {
@@ -18,31 +18,31 @@ function draw() {
             if (x % (2 * SQUARE_SIZE) > SQUARE_SIZE) {
                 ellipse(x - (STEP / 2), y + progression - (STEP / 2), circlesize, circlesize);
             } else {
-                ellipse(x, y, circlesize, circlesize);
+                ellipse(x, y + progression, circlesize, circlesize);
 
             }
         }
     }
 
-    // // weft
-    // fill('black');
-    //
-    // for (let x = -20; x < width + 20; x += STEP) {
-    //     for (let y = -20; y < height + 20; y += STEP) {
-    //         if (y % (2 * SQUARE_SIZE) > SQUARE_SIZE) {
-    //             ellipse(x + progression, y - (STEP / 2), circlesize, circlesize);
-    //         } else {
-    //             ellipse(x + progression - (STEP / 2), y, circlesize, circlesize);
-    //
-    //         }
-    //     }
-    // }
+    // weft
+    fill('black');
+
+    for (let x = -20; x < width + 20; x += STEP) {
+        for (let y = -20; y < height + 20; y += STEP) {
+            if (y % (2 * SQUARE_SIZE) > SQUARE_SIZE) {
+                ellipse(x + progression, y - (STEP / 2), circlesize, circlesize);
+            } else {
+                ellipse(x + progression - (STEP / 2), y, circlesize, circlesize);
+
+            }
+        }
+    }
 
 
     // SECOND LAYER
     circlesize = (1 + -Math.cos((progression / STEP) * Math.PI * 2) * 0.5) * 15;
 
-    fill('white');
+    fill('blue');
     noStroke();
     // warp
     for (let x = -20; x < width + 20; x += STEP) {
@@ -50,25 +50,25 @@ function draw() {
             if (x % (2 * SQUARE_SIZE) > SQUARE_SIZE) {
                 ellipse(x, y + progression, circlesize, circlesize);
             } else {
-                ellipse(x + (STEP / 2), y + (STEP / 2), circlesize, circlesize);
+                ellipse(x - (STEP / 2), y - (STEP / 2) + progression, circlesize, circlesize);
 
             }
         }
     }
 
-    // // weft
-    // fill('gray');
-    //
-    // for (let x = -20; x < width + 20; x += STEP) {
-    //     for (let y = -20; y < height + 20; y += STEP) {
-    //         if (y % (2 * SQUARE_SIZE) > SQUARE_SIZE) {
-    //             ellipse(x + progression, y - (STEP / 2), circlesize, circlesize);
-    //         } else {
-    //             ellipse(x + progression - (STEP / 2), y, circlesize, circlesize);
-    //
-    //         }
-    //     }
-    // }
+    // weft
+    fill('SlateBlue');
+
+    for (let x = -20; x < width + 20; x += STEP) {
+        for (let y = -20; y < height + 20; y += STEP) {
+            if (y % (2 * SQUARE_SIZE) > SQUARE_SIZE) {
+                ellipse(x + progression - (STEP / 2), y, circlesize, circlesize);
+            } else {
+                ellipse(x + progression, y - (STEP / 2), circlesize, circlesize);
+
+            }
+        }
+    }
 
 
     progression += INCR;
